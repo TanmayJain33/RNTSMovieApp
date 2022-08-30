d8857c553f5511cfffd986e9b12c56d2d9f22192
TanmayJain33 <tanmay.jain@crownstack.com>
2022-08-30 12:53:47 +0530

    Refractor code

--- App.web.tsx
+++ App.web.tsx
@@ -1,37 +1,22 @@
-import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
 
 const App = () => {
-  const [count, setCount] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello from {'\n'}React Native Web!</Text>
-      <TouchableOpacity
-        onPress={() => setCount(count + 1)}
-        style={styles.button}>
-        <Text>Click me!</Text>
-      </TouchableOpacity>
-
-      <Text>You clicked {count} times!</Text>
+    <View style={styles.mainContainer}>
+      <Text style={styles.textStyles}>Hello</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    backgroundColor: '#C3E8BD',
-    paddingTop: 40,
-    paddingHorizontal: 10,
+    backgroundColor: '#151C26',
   },
-  button: {
-    backgroundColor: '#ADBDFF',
-    padding: 5,
-    marginVertical: 20,
-    alignSelf: 'flex-start',
-  },
-  title: {
-    fontSize: 40,
+  textStyles: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
 
